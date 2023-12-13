f5a2e57be0167184d70d758188c3dae1e0476c95
mycallCampbell <mycallcampbell@gmail.com>
2023-12-13 18:59:25 +0000

    added paths to urls for podcast views

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('blogs/', views.getBlogs, name='getBlogs'),
     path('blog/<str:pk>', views.getBlog, name='getBlog'),
+    path('podcasts/', views.getPodcasts, name='getPodcasts'),
+    path('podcast/<str:pk>', views.getPodcast, name='getPodcast'),
 ]
