b4bc144b67b3d951d0a9569aaafd7bee311db365
mycallCampbell <mycallcampbell@gmail.com>
2024-02-24 12:58:55 +0000

    updated ip address for new server

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.mycalldevcp.co.uk', '68.183.34.192']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.mycalldevcp.co.uk', '167.71.139.229']
 
 
 # Application definition
